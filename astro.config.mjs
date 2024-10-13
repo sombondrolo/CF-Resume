4b0183bbde317753fb61577a961242a94537217a
Fer <sombondrolo@gmail.com>
2024-10-13 15:00:37 -0300

    To test port 4321

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   server:{
-		port: 3000,
+		// port: 3000,
+    port: 4321,
 		host: true
 	}
 }
