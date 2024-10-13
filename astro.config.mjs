7f3127768369d03ebab60a5c35ee55f9f7a8dbaa
Fer <sombondrolo@gmail.com>
2024-10-13 14:32:59 -0300

    astro.config.mjs: added server port 3000 and host true from dokploy-astro-sample

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig(
-  {}
+export default defineConfig({
+  server:{
+		port: 3000,
+		host: true
+	}
+}
 )
